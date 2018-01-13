7b8b821da19ad9c4cb925b0662cf1c95d39e2659
Richard Loera <richexp9@gmx.com>
2018-01-13 05:17:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(message)
-  if message === message.upcase
-    return "NO, NOT SINCE 1938!"
-  end
   if message === "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif message === message.upcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
